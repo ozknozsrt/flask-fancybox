d74a082d8df3f3c9297023c227bd510c3ff39c22
Özkan ÖZSERT <ozkanozsert@gmail.com>
2017-10-18 04:56:58 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')  
+def home():  
+    pics = os.listdir('static/images/')  
+    return render_template("index.html", pics=pics)
